99d5c5d2808bca9391f43c8dca4f9d5b0b3fb7a5
Ben Davison <benpdavison@gmail.com>
2019-02-19 13:21:19 +0000

    revert test

--- src/test/java/org/jenkinsci/plugins/workflow/support/steps/input/InputStepTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/support/steps/input/InputStepTest.java
@@ -25,6 +25,7 @@
 package org.jenkinsci.plugins.workflow.support.steps.input;
 
 import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.google.common.base.Predicate;
@@ -362,31 +363,5 @@ public class InputStepTest extends Assert {
         p.setDefinition(new CpsFlowDefinition("timeout(time: 1, unit: 'SECONDS') {input message: 'OK?', submitter: 'ops'}", true));
         j.assertBuildStatus(Result.ABORTED, p.scheduleBuild2(0).get());
     }
-
-  @Test
-  @Issue("JENKINS-51490")
-  public void test_abort_parameter() throws Exception {
-    //set up dummy security real
-    j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
-    // job setup
-    WorkflowJob foo = j.jenkins.createProject(WorkflowJob.class, "foo");
-    foo.setDefinition(new CpsFlowDefinition(StringUtils.join(Arrays.asList(
-        "def x = input message:'Do you want chocolate?', id:'Icecream', ok: 'Purchase icecream', " +
-            "abort: 'Do not purchase icecream', submitterParameter: 'alice';",
-        "echo(\"after: ${x}\");"),"\n"),true));
-
-    // get the build going, and wait until workflow pauses
-    QueueTaskFuture<WorkflowRun> q = foo.scheduleBuild2(0);
-    WorkflowRun b = q.getStartCondition().get();
-    j.waitForMessage("input", b);
-
-    // make sure we are pausing at the right state that reflects what we wrote in the program
-    InputAction a = b.getAction(InputAction.class);
-    assertEquals(1, a.getExecutions().size());
-
-    InputStepExecution is = a.getExecution("Icecream");
-    assertEquals("Do not purchase icecream", is.getInput().getAbort());
-
-  }
 
 }
